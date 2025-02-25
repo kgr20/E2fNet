519435575f12641b6244d28f5fd611c064048eda
acb11155on <huuquan1994@gmail.com>
2025-02-25 14:36:00 +0900

    config for h5 data roots

---
+++ data_cfg.py
@@ -0,0 +1,6 @@
+# path to h5 data roots after datasets pre-processing. See docs/datasets_howto.md
+data_roots = {
+    "NODDI": "/home/Datasets/EEG2fMRI/NODDI_h5_data",
+    "Oddball": "/home/Datasets/EEG2fMRI/Oddball_h5_data",
+    "CNEPFL": "/home/Datasets/EEG2fMRI/CNEPFL_h5_data",
+}
