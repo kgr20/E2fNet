6d59b3a500efe9d63bc8b2e26b5889b2ad18a30b
acb11155on <huuquan1994@gmail.com>
2025-02-25 14:37:32 +0900

    training scripts

---
+++ run_train.sh
@@ -0,0 +1,13 @@
+### Individual naming/format
+# NODDI: [32, 35, ..., 49, 50]
+# Oddball: [sub001, sub002, ..., sub016, sub017]
+# CNEPFL: [sub-02, sub-04, ..., sub-24, sub-26]
+
+python train_E2fNet.py \
+--dataset NODDI \
+--test_ids 43 \
+--fmri_channel 30 \
+--exp_root /home/Experiments/EEG2fMRI \
+--batch_size 32 \
+--num_epochs 50 \
+--lr 0.001
