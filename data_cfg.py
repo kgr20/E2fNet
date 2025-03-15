48b17106a0db7b597aefd1e1b468df6697cfed9d
Quan Cap <huuquan1994@gmail.com>
2025-03-15 13:58:27 +0900

    update data cfg

--- data_cfg.py
+++ data_cfg.py
@@ -1,6 +1,35 @@
-# path to h5 data roots after datasets pre-processing. See docs/datasets_howto.md
-data_roots = {
-    "NODDI": "/home/Datasets/EEG2fMRI/NODDI_h5_data",
-    "Oddball": "/home/Datasets/EEG2fMRI/Oddball_h5_data",
-    "CNEPFL": "/home/Datasets/EEG2fMRI/CNEPFL_h5_data",
+# Parameters to process the raw datasets
+processing_cfg = {
+    'bold_shift': 6,
+    'eeg_limit': True,
+    'eeg_f_limit': 250,
+    'interval_eeg': 20,
+
+    "NODDI": {
+        'n_volumes': 294, # 300 - bold_shift
+        'f_resample': 2.160,
+    },
+
+    "Oddball": {
+        'n_volumes': 164, # 170 - bold_shift
+        'f_resample': 2.0,
+    },
+    
+    "CNEPFL": {
+        'n_volumes': 364, # 370 - bold_shift
+        'f_resample': 1.280,
+    },
+}
+
+raw_data_roots = {
+    "NODDI": "/data/EEG2fMRI/NODDI/",
+    "Oddball": "/data/EEG2fMRI/Oddball/",
+    "CNEPFL": "/data/EEG2fMRI/CN-EPFL/ds002158-download/",
+}
+
+# Path to h5 data roots after datasets pre-processing. See docs/datasets_howto.md
+processed_data_roots = {
+    "NODDI": "/data/EEG2fMRI/NODDI_h5_data",
+    "Oddball": "/data/EEG2fMRI/Oddball_h5_data",
+    "CNEPFL": "/data/EEG2fMRI/CNEPFL_h5_data",
 }
