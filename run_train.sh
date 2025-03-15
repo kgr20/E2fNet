f8a4a4ca85f48fe51497dfe8b2de9795fae1bd0d
Quan Cap <huuquan1994@gmail.com>
2025-03-15 18:24:48 +0900

    minor updates

--- run_train.sh
+++ run_train.sh
@@ -3,7 +3,8 @@
 # Oddball: [sub001, sub002, ..., sub016, sub017]
 # CNEPFL: [sub-02, sub-04, ..., sub-24, sub-26]
 
-python train_E2fNet.py \
+## To train E2fNet
+CUDA_VISIBLE_DEVICES=0 python train_E2fNet.py \
 --dataset NODDI \
 --test_ids 43 \
 --fmri_channel 30 \
@@ -11,3 +12,13 @@ python train_E2fNet.py \
 --batch_size 32 \
 --num_epochs 50 \
 --lr 0.001
+
+# ## To train E2fGAN
+# CUDA_VISIBLE_DEVICES=0 python train_E2fGAN.py \
+# --dataset NODDI \
+# --test_ids 43 \
+# --fmri_channel 30 \
+# --exp_root /home/Experiments/EEG2fMRI \
+# --batch_size 32 \
+# --num_epochs 50 \
+# --lr 0.001
